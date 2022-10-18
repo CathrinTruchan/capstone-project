ea966dd0079753e53cbf7562bf721dfb1b6c13c4
Cathrin Truchan <cathrin.truchan@gmail.com>
2022-10-18 12:01:27 +0200

    add minor changes in global styles

--- components/Asana-Card/AsanaCard.js
+++ components/Asana-Card/AsanaCard.js
@@ -8,8 +8,8 @@ export default function AsanaCard({ name, img }) {
         <Image
           src={img}
           alt={name}
-          width={100}
-          height={100}
+          width={1000}
+          height={1000}
           layout="responsive"
         />
       </ImageWrapper>
