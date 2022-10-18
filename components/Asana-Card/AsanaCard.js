a38429cb5d966ff01ab4ed29770fce0a08053f93
Cathrin Truchan <cathrin.truchan@gmail.com>
2022-10-18 11:40:28 +0200

    add local image folder and add Image Element to AsanaCard Component

--- components/Asana-Card/AsanaCard.js
+++ components/Asana-Card/AsanaCard.js
@@ -1,11 +1,24 @@
 import styled from "styled-components";
 import Image from "next/image";
 
-export default function AsanaCard({ name }) {
+export default function AsanaCard({ name, img }) {
   return (
     <section>
-      <Image src="" alt={name} width={500} height={500} layout="responsive" />
+      <ImageWrapper>
+        <Image
+          src={img}
+          alt={name}
+          width={100}
+          height={100}
+          layout="responsive"
+        />
+      </ImageWrapper>
       <h2>{name}</h2>
     </section>
   );
 }
+
+const ImageWrapper = styled.section`
+  width: 100px;
+  height: 100px;
+`;
