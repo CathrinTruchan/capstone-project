30deaa17f047d012df95bfff47aa8b234c1cf10f
Cathrin Truchan <cathrin.truchan@gmail.com>
2022-10-19 15:04:16 +0200

    add list of all yoga poses

--- pages/flow/index.js
+++ pages/flow/index.js
@@ -2,21 +2,22 @@ import styled from "styled-components";
 import AsanaCard from "../../components/Asana-Card/AsanaCard";
 import { getAsanaByID } from "../../db";
 import { useState } from "react";
+import asanas from "../../db";
 
 export default function Flow() {
-  const [flowAsanaIds, setFlowAsanaIds] = useState(["5", "2", "3", "4"]);
+  const [flowAsanaIds, setFlowAsanaIds] = useState([]);
 
-  const flowAsanas = flowAsanaIds.map((item) => {
-    return getAsanaByID(item);
+  const flowAsanas = flowAsanaIds.map((id) => {
+    return getAsanaByID(id);
   });
-
+  console.log(flowAsanas);
   return (
     <>
       <h2>Your flow for today:</h2>
       <StyledList>
-        {flowAsanas.map((asana) => {
+        {flowAsanas.map((asana, index) => {
           return (
-            <li key={asana.id}>
+            <li key={index}>
               <AsanaCard
                 name={asana.english_name}
                 img={asana.img_url}
@@ -26,6 +27,23 @@ export default function Flow() {
           );
         })}
       </StyledList>
+
+      <section>
+        <ul>
+          {asanas.map((asana) => {
+            return (
+              <li key={asana.id}>
+                <p>{asana.english_name}</p>
+                <button
+                  onClick={() => setFlowAsanaIds([...flowAsanaIds, asana.id])}
+                >
+                  Add to flow
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </>
   );
 }
