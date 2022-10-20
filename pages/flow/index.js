7bedcc6f543d0054a7d486bf7760ac560cbb7e06
Cathrin Truchan <cathrin.truchan@gmail.com>
2022-10-20 11:31:57 +0200

    add basic styling to AddAsana-Section

--- pages/flow/index.js
+++ pages/flow/index.js
@@ -8,12 +8,18 @@ export default function Flow() {
   const [flowAsanaIds, setFlowAsanaIds] = useState([]);
   const [open, setOpen] = useState(false);
 
+  function toggleOpenState() {
+    if (open) {
+      return false;
+    } else return true;
+  }
+
   const flowAsanas = flowAsanaIds.map((id) => {
     return getAsanaByID(id);
   });
 
   return (
-    <>
+    <StyledContainer>
       <StyledH2>Your Flow for today:</StyledH2>
 
       <StyledList>
@@ -28,13 +34,13 @@ export default function Flow() {
         ))}
       </StyledList>
 
-      <StyledButton onClick={() => setOpen(true)}>
+      <StyledButton onClick={() => setOpen(toggleOpenState)}>
         Create your Flow
       </StyledButton>
-      <p onClick={() => setOpen(false)}>X</p>
 
       {open && (
         <AddAsanaSection>
+          <CloseButton onClick={() => setOpen(false)}>X</CloseButton>
           <StyledList>
             {asanas.map((asana) => (
               <StyledListItem key={asana.id}>
@@ -42,14 +48,14 @@ export default function Flow() {
                 <StyledAddButton
                   onClick={() => setFlowAsanaIds([...flowAsanaIds, asana.id])}
                 >
-                  <a>Add to flow</a>
+                  <a>+</a>
                 </StyledAddButton>
               </StyledListItem>
             ))}
           </StyledList>
         </AddAsanaSection>
       )}
-    </>
+    </StyledContainer>
   );
 }
 
@@ -61,24 +67,37 @@ const StyledListItem = styled.li`
   display: flex;
   gap: 2rem;
   justify-content: space-between;
+  align-items: center;
 `;
 
-const StyledAddButton = styled.button`
+const StyledAddButton = styled.div`
   all: unset;
   cursor: pointer;
+  background-color: #5d6bea;
+  color: #f5f5f5;
+  padding: 0;
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 50%;
+  text-align: center;
   &:active {
-    color: orange;
+    background-color: orange;
   }
 `;
 
 const AddAsanaSection = styled.section`
-  width: 400px;
-  height: 400px;
+  width: 500px;
+  height: 90vh;
   overflow-y: scroll;
   margin: 1.5rem auto;
-  background-color: #f5f5f5;
+  background-color: #ffffff;
   border-radius: 22px;
   padding: 1.5rem;
+  position: absolute;
+  top: 1rem;
+  left: 4rem;
+  z-index: 5;
+  box-shadow: 0px 0px 4px rgba(93, 107, 234, 0.42);
 `;
 
 const StyledH2 = styled.h2`
@@ -94,4 +113,19 @@ const StyledButton = styled.button`
   color: #f5f5f5;
   padding: 0.5rem 3rem;
   border-radius: 22px;
+`;
+
+const StyledContainer = styled.section`
+  position: relative;
+`;
+
+const CloseButton = styled.div`
+  background-color: #5d6bea;
+  color: #f5f5f5;
+  padding: 0;
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 50%;
+  text-align: center;
+  cursor: pointer; ;
 `;
