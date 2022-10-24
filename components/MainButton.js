9340ed533ceab41c93b5790b1836fb151d7f0610
Cathrin Truchan <cathrin.truchan@gmail.com>
2022-10-24 11:25:31 +0200

    add MainButton component to components folder

---
+++ components/MainButton.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const MainButton = styled.button`
+  width: 16rem;
+  display: block;
+  padding: 1rem 3rem;
+  border-radius: 22px;
+  font-size: 1.1rem;
+  cursor: pointer;
+  box-shadow: ${({ type }) =>
+    type === "primary" ? "var(--drop-shadow-gray)" : "none"};
+  border: ${({ type }) =>
+    type === "primary" ? "none" : " 2px solid var(--highlight)"};
+  color: ${({ type }) =>
+    type === "primary" ? "var(--text-light)" : "var(--highlight)"};
+  background: ${({ type }) =>
+    type === "primary" ? "var(--highlight-gradient)" : "white"};
+  &:hover {
+    background: ${({ type }) =>
+      type === "primary" ? "var(--highlight)" : "var(--background-neutral)"};
+  }
+`;
+
+export { MainButton };
